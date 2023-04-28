c835d0dfb229f3c649e914248aab7f4a24f32f68
Paul POINSINET DE SIVRY <ppoinsin@e2r2p22.clusters.42paris.fr>
2023-04-28 11:09:14 +0200

    github repo fix

--- P3/src/create-app.sh
+++ P3/src/create-app.sh
@@ -1,7 +1,7 @@
 kubectl patch svc argocd-server -n argocd --patch-file patch.yaml
 
 argocd login localhost:30002
-argocd app create ppoinsin-app --repo https://github.com/Ppoinsinet/ppoinsin-badass-config.git --dest-server https://kubernetes.default.svc --dest-namespace dev --path  deploy
+argocd app create ppoinsin-app --repo https://github.com/Ppoinsinet/ppoinsin-IOT-config.git --dest-server https://kubernetes.default.svc --dest-namespace dev --path  deploy
 argocd app set ppoinsin-app --sync-policy automated --sync-option ApplyOutOfSyncOnly=true --self-heal
 
 kubectl apply -f ingress.yml -n dev
